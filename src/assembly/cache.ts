1b45598962c5ecc930dcb02eac03532c19ee619b
Nidin Vinayakan <01@01alchemist.com>
2019-06-28 13:52:21 +0200

    Removed segments from cache

--- src/assembly/cache.ts
+++ src/assembly/cache.ts
@@ -1,17 +1,8 @@
 type MapT = Map<string, string>;
-class Segment {
-  locked: boolean = false;
-  constructor(public map: MapT) {}
-}
 const addr = 0;
 export class Cache {
   lock: i32 = 0;
   map: Map<string, string>;
-  // segmentMap: Map<string, i32>;
-  // segments: Segment[];
-  // constructor(segments: Segment[] = null) {
-  //   this.init(segments);
-  // }
   constructor(map: MapT | null = null) {
     this.init(map);
   }
@@ -22,31 +13,6 @@ export class Cache {
       this.map = map;
     }
   }
-  // init(segments: Segment[] = null): void {
-  //   if (segments === null) {
-  //     this.segments = [];
-  //     for (let i: i32 = 0; i < 16; i++) {
-  //       let map = new Map<string, string>();
-  //       this.segments[i] = new Segment(map);
-  //     }
-  //   } else {
-  //     this.segments = segments;
-  //   }
-  // }
-  // private get map(): MapT {
-  //   let locked = false;
-  //   while (!locked) {
-  //     for (let i: i32 = 0; i < 16; i++) {
-  //       let seg = this.segments[i];
-  //       if (!seg.locked) {
-  //         // atomic.
-  //         return seg.map;
-  //       }
-  //     }
-  //     locked = true;
-  //   }
-  //   return null;
-  // }
   has(key: string): bool {
     return this.map.has(key);
   }
@@ -55,6 +21,8 @@ export class Cache {
     return data;
   }
   set(key: string, value: string): void {
+    // lock(addr)
     this.map.set(key, value);
+    // unlock(addr)
   }
 }
